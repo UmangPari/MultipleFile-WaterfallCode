08e0d4c879c01bebfdece7edcbebfa6e659f32d6
Roopam001 <66717302+Roopam001@users.noreply.github.com>
2020-09-10 15:01:54 +0530

    Update alertRespondDialog.js

--- dialogs/alertRespondDialog.js
+++ dialogs/alertRespondDialog.js
@@ -17,7 +17,14 @@ const {
 } = require('botbuilder-dialogs');
 const { HealthRulesDialog }=require('./healthRulesDialog');
 const { ActionSuppressionDialog }=require('./actionSuppressionDialog');
+const { PolicyDialog }=require('./policyDialog');
+const { EmailDigestDialog }=require('./emailDigestDialog');
+const { ActionDialog }=require('./actionDialog');
 
+var recurFlag;
+const ACTION_DIALOG='actionDialog';
+const EMAILDIGEST_DIALOG='emailDigestDialog';
+const POLICY_DIALOG='policyDialog';
 const HEALTHRULES_DIALOG='healthRulesDialog';
 const ACTIONSUPPRESSION_DIALOG='actionSuppressionDialog';
 
@@ -33,9 +40,14 @@ class AlertRespondDialog extends ComponentDialog {
             .addDialog(new ChoicePrompt(CHOICE_PROMPT))
             .addDialog(new HealthRulesDialog(HEALTHRULES_DIALOG))
             .addDialog(new ActionSuppressionDialog(ACTIONSUPPRESSION_DIALOG))
+            .addDialog(new PolicyDialog(POLICY_DIALOG))
+            .addDialog(new EmailDigestDialog(EMAILDIGEST_DIALOG))
+            .addDialog(new ActionDialog(ACTION_DIALOG))
             .addDialog(new WaterfallDialog(WATERFALL_DIALOG, [
                 this.alertTypeStep.bind(this),
-                this.actionStep.bind(this)
+                this.actionStep.bind(this),
+                this.confirmStep.bind(this),
+                this.finalStep.bind(this)
         ]));
 
         this.initialDialogId = WATERFALL_DIALOG;
@@ -44,26 +56,27 @@ class AlertRespondDialog extends ComponentDialog {
     {   
         return await step.prompt(CHOICE_PROMPT, {
             prompt: 'Please choose the following actions',
-            choices: ChoiceFactory.toChoices(['Policies','Health Rules','Anomaly Detection','Actions','Action Suppression','Email Digests'])
+            choices: ChoiceFactory.toChoices(['Policies','Health Rules','Actions','Action Suppression','Email Digests','Main Menu','BACK'])
         });
-    }
+       }
     async actionStep(step)
     {
       if(step.result.value=='Policies')
       {
-
+        return await step.beginDialog(POLICY_DIALOG);
       }
       else if(step.result.value=='Health Rules')
       {
           return await step.beginDialog(HEALTHRULES_DIALOG);
       }
-      else if(step.result.value=='Anomaly Detection')
+     /* else if(step.result.value=='(Anomaly Detection)')
       {
-          
-      }
+        step.context.sendActivity("Work in Progress");
+        return await step.next();
+      }*/
       else if(step.result.value=='Actions')
-      {
-          
+      { 
+        return await step.beginDialog(ACTION_DIALOG);
       }
       else if(step.result.value=='Action Suppression')
       {
@@ -71,12 +84,61 @@ class AlertRespondDialog extends ComponentDialog {
       }
       else if(step.result.value=='Email Digests')
       {
+        return await step.beginDialog(EMAILDIGEST_DIALOG);
+      }
+      else if(step.result.value=='Main Menu')
+      {
+          return await step.endDialog(0);
+      }
+      else if(step.result.value=='BACK')
+      {
+        return await step.endDialog(1);
+      }
+      else
+      {
+        return await step.next();
+      }
+  }
+  async confirmStep(step)
+  {
+      if(step.result==1)
+      {
+          return await step.beginDialog('alertRespondDialog');
           
       }
-      else{}
-      return await step.endDialog();
-    
-  }
+      else if(step.result==0)
+      {
+          return await step.endDialog(0);
+      }
+      else
+      {
+        return await step.prompt(CHOICE_PROMPT, {
+           prompt: 'Any more Info about Alert and respond?',
+           choices: ChoiceFactory.toChoices(['yes', 'no'])
+      });
+    }
+  } 
+
+  async finalStep(step)
+  {
+      if(step.result==0)
+      {
+          return await step.endDialog(0);
+      }
+      else if(step.result==1)
+      {
+          return await step.endDialog(0);
+      }
+      else if(step.result.value=='yes')
+      {
+         return await step.beginDialog('alertRespondDialog');
+      }
+      else 
+      {   
+          return await step.endDialog();
+      }
+      
+  } 
 
    }
 
