b9e93b7770b55f47642c961079f01911d7af70ae
Roopam001 <66717302+Roopam001@users.noreply.github.com>
2020-09-10 15:10:07 +0530

    Update reportsDialog.js

--- dialogs/reportsDialog.js
+++ dialogs/reportsDialog.js
@@ -21,12 +21,14 @@ const { AppNameDialog }=require('./appNameDialog');
 const APPNAME_DIALOG = 'appNameDialog';
 
 
-var appdLink='https://chaplin202008130019254.saas.appdynamics.com';
-var appdUserName='chaplin202008130019254@chaplin202008130019254';
-var appdPassword='lb19y0vkgnwf';
+var appdLink='https://charlie202008310330195.saas.appdynamics.com';
+var appdUserName='charlie202008310330195@charlie202008310330195';
+var appdPassword='5myrxxro74q7';
 
 var inputApp='';
 var info='';
+var totalApp='';
+var appList = new Array();
 
 const CHOICE_PROMPT = 'choicePrompt';
 const TEXT_PROMPT = 'textPrompt';
@@ -42,7 +44,9 @@ class ReportsDialog extends ComponentDialog {
             .addDialog(new WaterfallDialog(WATERFALL_DIALOG, [
                 this.quesStep.bind(this),
                 this.appStep.bind(this),
-                this.actionStep.bind(this)
+                this.actionStep.bind(this),
+                this.confirmStep.bind(this),
+                this.finalStep.bind(this)    
         ]));
 
         this.initialDialogId = WATERFALL_DIALOG;
@@ -51,7 +55,7 @@ class ReportsDialog extends ComponentDialog {
     {
         return await step.prompt(CHOICE_PROMPT, {
             prompt: 'Please choose the Report u want to know.',
-            choices: ChoiceFactory.toChoices(['All App agent Version'])
+            choices: ChoiceFactory.toChoices(['All App agent Version','Main Menu','BACK'])
               });
 
     }
@@ -59,7 +63,29 @@ class ReportsDialog extends ComponentDialog {
     async appStep(step)
     {
         info= step.result.value;
-        return await step.beginDialog(APPNAME_DIALOG);
+        if(info=='Main Menu'||info=='BACK')
+        {
+            return await step.next();
+        }
+        else
+        {
+            await axios.get(`${appdLink}/controller/rest/applications?output=json`,
+            {
+            auth:
+            {
+                username: appdUserName,
+                password: appdPassword
+            }
+            }).then((result) =>{   
+            totalApp=result.data;
+            });  
+            
+            for(var i=0;i<totalApp.length;i++)
+            {
+            appList[i]=totalApp[i].name;
+            }
+            return await step.next();
+        }    
     }
 
     async actionStep(step)
@@ -67,23 +93,74 @@ class ReportsDialog extends ComponentDialog {
         inputApp=step.result;
 
         if(info=='All App agent Version')
-        {
-            await axios.get(`${appdLink}/controller/rest/applications/${inputApp}/nodes?output=json`,
+        {   
+            for(var i=0;i<appList.length;i++)
             {
-               auth:
+                await axios.get(`${appdLink}/controller/rest/applications/${appList[i]}/nodes?output=json`,
                 {
-                    username: appdUserName,
+                    auth:
+                    {
+                         username: appdUserName,
+                        password: appdPassword
+                    }
+                }).then((result) => 
+                     {  
+                         step.context.sendActivity("App Name :"+ appList[i]);
+                          step.context.sendActivity("App agent version: " + result.data[0].appAgentVersion);
+                          step.context.sendActivity("machine agent version :" + result.data[0].machineAgentVersion);
+                    });
+            }
+            await axios.get(`${appdLink}//controller/rest/applications/_dbmon/nodes?output=json`,
+            {
+                auth:
+                {
+                     username: appdUserName,
                     password: appdPassword
-                 }
-            }).then((result) => 
-                 {
-                      step.context.sendActivity(result.data[0].appAgentVersion);
-                });
+                }
+            }).then((result) =>
+            {
+                for(var i=0;i<result.data.length;i++)
+                {
+                    step.context.sendActivity('Db agent name is :'+ result.data[i].name+' and its version is: '+result.data[i].appAgentVersion);
+                    
+                }
+            });
+            
+        
+            return await step.next();
         }
+        else if(info=='Main Menu')
+        {
+            return await step.endDialog(0);
+        }
+        else if(info=='BACK')
+        {
+            return await step.endDialog(1);
+        }    
       
-      return await step.endDialog();
+      
     
   }
+  async confirmStep(step)
+  {
+      return await step.prompt(CHOICE_PROMPT, {
+          prompt: 'Any more Info about Reports?',
+          choices: ChoiceFactory.toChoices(['yes', 'no'])
+      });
+  } 
+
+  async finalStep(step)
+  {
+      if(step.result.value=='yes')
+      {
+         return await step.beginDialog('reportsDialog');
+      }
+      else
+      {   
+          step.context.sendActivity('Bye');
+          return await step.endDialog();
+      }
+  } 
 
    }
 
